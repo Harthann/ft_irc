f0af0efe28d8def465a745badd6739e6f02e62aa
bsteve456 <stevealan1@protonmail.com>
2021-03-27 15:38:02 +0100

    add Member and constructor to user(not finish)

--- include/classes/User.hpp
+++ include/classes/User.hpp
@@ -2,17 +2,26 @@
 #define USER_HPP
 
 #include "Socket.hpp"
+#include <string.h>
+
 
 class User
 {
 	public:
 		User();
+		User(Socket *client, std::string datas);
 		~User();
+		Socket *	getSocket() const;
+		int			getStatus() const;
 	protected:
 	private:
 		std::string	nickname;
+		void		setNICK(char *str);
+		void		setPASS(char *str);
+		void		setUSER(char *str);
 		std::string	user;
 		int			mode;
+		int status;
 		std::string	realname;
 		std::string	password;
 		Socket		*self;
--- srcs/classes/User.cpp
+++ srcs/classes/User.cpp
@@ -2,10 +2,31 @@
 
 User::User()
 {
+}
 
+User::User(Socket *client, std::string datas)
+{
+	this->self = client;
+	this->status = 0;
+	char *str;
+
+	str = datas.c_str();
+	if(!strncmp(str, "PASS", 4))
+		this->setPASS(str + 4);
+	else if(!strncmp(str, "NICK", 4))
+		this->setNICK(str + 4);
+}
+
+Socket * User::getSocket() const
+{
+	return this->self;
+}
+
+int 	User::getStatus() const
+{
+	return this->status;
 }
 
 User::~User()
 {
-
 }
