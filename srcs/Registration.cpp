b3d58912d2f3c3c55dc0e57bb97087495b82d64f
bsteve456 <stevealan1@protonmail.com>
2021-04-28 14:16:02 +0200

    add almost working channel

--- srcs/Registration.cpp
+++ srcs/Registration.cpp
@@ -7,8 +7,8 @@ void	validation(std::vector<User> &temp_user, Socket *client)
 		if(temp_user[i].getSocket() == client)
 		{
 			std::cerr << "HERE" << std::endl;
-			client->Confirm("001  RPL_WELCOME Welcome to the Internet Relay Network " + temp_user[i].getNickname() + "!" + temp_user[i].getUser() + "@127.0.0.1");
-			client->Send("001 RPL_WELCOME Welcome to the Internet Relay Network " + temp_user[i].getNickname() + "!" + temp_user[i].getUser() + "@localhost");
+	//		client->Confirm("001  RPL_WELCOME Welcome to the Internet Relay Network " + temp_user[i].getNickname() + "!" + temp_user[i].getUser() + "@127.0.0.1");
+			client->Send("001 RPL_WELCOME   Welcome to the Internet Relay Network " + temp_user[i].getNickname() + "!" + temp_user[i].getUser() + "@localhost\n");
 
 			break;
 		}
@@ -31,7 +31,7 @@ void	add_user(Socket *client, std::vector<User> &temp_user, Commands &cmd)
 		temp_user[i].setDatas(cmd);
 	else if(n == 0)
 		temp_user.push_back(User(client, cmd));
-//	client->Send("001 RPL [USER_NAME] Welcome to the server");
+//	client->Send("001 RPL_WELCOME [USER_NAME] Welcome to the server\n");
 //	client->Confirm("001 WELCOME_MSG [USER_NAME] Welcome to the server");
 	validation(temp_user, client);
 }
@@ -42,8 +42,7 @@ int		already_register(Socket *client, Server &server)
 	{
 		if(server.getClients()[i].getSocket() == client)
 		{
-			client->Send("001 RPL_WELCOME Welcome to the Internet Relay Network " + server.getClients()[i].getNickname() + "!" + server.getClients()[i].getUser() + "@localhost ");
-
+//			client->Send("001 RPL_WELCOME Welcome to the Internet Relay Network " + server.getClients()[i].getNickname() + "!" + server.getClients()[i].getUser() + "@localhost\n");
 			return (1);
 		}
 	}
