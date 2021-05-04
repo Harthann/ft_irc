4d1bd2fa9e2e00a4519afce0e5b98c9fe0041e01
bsteve456 <stevealan1@protonmail.com>
2021-05-04 16:36:54 +0200

    add Join message modif

--- srcs/classes/Channel.cpp
+++ srcs/classes/Channel.cpp
@@ -9,6 +9,7 @@ Channel::Channel(std::string Name, User &C_operator) : name(Name), topic(""), se
 {
 	this->channel_operator = &C_operator;
 	this->active_users.push_back(C_operator);
+	this->list_all_users = "@" + C_operator.getNickname();
 	std::string temp = ":" + C_operator.getUser() + "!~" + C_operator.getUser() + "@127.0.0.1";
 	std::string msg = temp + " JOIN :"+ Name + "\n";
 	C_operator.getSocketPtr()->bufferize(msg);
@@ -17,7 +18,7 @@ Channel::Channel(std::string Name, User &C_operator) : name(Name), topic(""), se
 		msg = ":" + server_name + " 332 " + C_operator.getUser() + " " + name + " :" + topic + "\n";
 		C_operator.getSocketPtr()->bufferize(msg);
 	}
-	msg = ":" + server_name + " 353 " + C_operator.getUser() + " = " + this->name + " :@" + C_operator.getNickname() + "\n";
+	msg = ":" + server_name + " 353 " + C_operator.getUser() + " = " + this->name + " :" + this->list_all_users + "\n";
 	C_operator.getSocketPtr()->bufferize(msg);
 	msg = ":" + server_name + " 366 " + C_operator.getUser() + " " + this->name + " :End of NAMES list.\n";
 	C_operator.getSocketPtr()->bufferize(msg);
@@ -26,6 +27,8 @@ Channel::Channel(std::string Name, User &C_operator) : name(Name), topic(""), se
 void				Channel::addUser(User &user)
 {
 	this->active_users.push_back(user);
+	this->list_all_users +=  ", " + user.getNickname();
+
 	std::string temp = ":" + user.getUser() + "!~" + user.getUser() + "@127.0.0.1";
 	std::string msg = temp + " JOIN :"+ this->name + "\n";
 	user.getSocketPtr()->bufferize(msg);
@@ -34,7 +37,7 @@ void				Channel::addUser(User &user)
 		msg = ":" + server_name + " 332 " + user.getUser() + " " + this->name + " :" + this->topic + "\n";
 		user.getSocketPtr()->bufferize(msg);
 	}
-	msg = ":" + server_name + " 353 " + user.getUser() + " = " + this->name + " :" + this->list_all_users();
+	msg = ":" + server_name + " 353 " + user.getUser() + " = " + this->name + " :" + this->list_all_users + "\n";
 	user.getSocketPtr()->bufferize(msg);
 	msg = ":" + server_name + " 366 " + user.getUser() + " " + this->name + " :End of NAMES list.\n";
 	user.getSocketPtr()->bufferize(msg);
@@ -44,21 +47,6 @@ void				Channel::addUser(User &user)
 std::string			Channel::getName()
 {
 	return this->name;
-}
-
-std::string		Channel::list_all_users()
-{
-	std::string user_list;
-
-	for (unsigned int i = 0; i < this->active_users.size(); ++i)
-	{
-		user_list.append(active_users[i].getUser());
-		if(i + 1 < this->active_users.size())
-			user_list.append(", ");
-		else
-			user_list.append("\n");
-	}
-	return user_list;
 }
 
 Channel::~Channel()
