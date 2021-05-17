22a1b946eda6617dc15421b32d388e16e50ad7da
Antoine LOUBIERES <anloubie@e1r8p4.42.fr>
2021-05-17 15:34:56 +0200

    added comm proto and numerical replies numbers

---
+++ srcs/commands/topic.cpp
@@ -0,0 +1,27 @@
+#include "commands_prototypes.hpp"
+
+void	display_topic(Commands &cmd, Socket *client, Server &server) {
+	Channel		*chan;
+	std::string	response;
+	User		*current_user;
+
+	current_user = check_user(server.getClients(), client);
+	chan = channel_exist(cmd[1], server);
+	if (!chan)
+		response.append(chan->getName() + ":No such channel");
+	else
+	{
+		if (!(chan->getTopic().empty()))
+			response.append(":" + chan->getServerName() + " " + utils::itos(RPL_TOPIC) + " " + current_user->getNickname() + " " + chan->getName() + " :" + chan->getTopic());
+		else
+			response.append(":" + chan->getServerName() + " " + utils::itos(RPL_NOTOPIC) + " " + current_user->getNickname() + " " + chan->getName() + " :No topic is set.");
+	}
+	client->bufferize(response);
+}
+
+void	topic_command(Commands &cmd, Socket *client, Server &server) {
+	if (cmd.length() == 2)
+		display_topic(cmd, client, server);
+	else if (cmd.length() == 3)
+		set_topic(cmd, client, server);
+}
