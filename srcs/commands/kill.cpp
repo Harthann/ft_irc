920e6996705a72a6210928d268eddc1adb24a916
Antoine LOUBIERES <anloubie@e1r7p8.42.fr>
2021-05-26 15:27:52 +0200

    added kill command for server or moderator use

---
+++ srcs/commands/kill.cpp
@@ -0,0 +1,17 @@
+#include "commands_prototypes.hpp"
+#include "utils.hpp"
+
+void	kill_by_server(Commands &cmd, Socket *client, Server &server, std::string msg) {
+	User	*user;
+
+	for (Server::user_vector::iterator it = server.getClients().begin(); it != server.getClients().end(); ++it) {
+		if ((*it)->getNickname() == cmd[1])
+		{
+			user = (*it);
+			user->partChannels();
+			server.delete_user(user, msg);
+			server.addUnavailableNickname(cmd[1]);
+			delete user;
+		}
+	}
+}
